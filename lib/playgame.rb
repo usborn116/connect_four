a2785534aa1ed0a0a779589f6e5ae6dc0b6cd55e
Usborn Ocampo <usborn116@gmail.com>
2022-07-01 14:27:28 -0700

    fixed bug that wouldn't allow a draw

--- lib/playgame.rb
+++ lib/playgame.rb
@@ -2,8 +2,8 @@ require_relative 'classes.rb'
 
 game1 = Game.new
 
-usborn = Player.new('Usborn', 0, game1, "\u{1F525}")
-ash = Player.new('Ashley', 1, game1, "\u{1F327}")
+usborn = Player.new('Usborn', 0, game1, "X")
+ash = Player.new('Ashley', 1, game1, "O")
 
 game1.add_player(usborn)
 game1.add_player(ash)
